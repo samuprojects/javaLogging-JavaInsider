4b91c3580d4b5b04e4315fd346580de638f77919
Samuel <samugit.projects@gmail.com>
2024-01-18 23:45:19 -0300

    Aula04 Baixando os arquivos JAR necessários

---
+++ src/theory/RequiredFiles.java
@@ -0,0 +1,20 @@
+package theory;
+
+@SuppressWarnings("ALL")
+public class RequiredFiles {
+
+    /*
+    *
+    * Acessado site https://search.maven.org digitar na busca slf4j-api e selecionar o do grupo org.slf4j Artifact ID slf4j-api
+    * Como o nosso projeto não está usando Maven vamos baixar o JAR em Downloads
+    *
+    * Depois na busca digitar logback-core, selecionar o do grupo ch.qos.logback e fazer o download
+    *
+    * Por último buscar por logback-classic, selecionar do mesmo grupo ch.qos.logback e fazer o download
+    *
+    * Após isso será feito a integração entre os JARs
+    *
+    *
+    *
+    * */
+}
