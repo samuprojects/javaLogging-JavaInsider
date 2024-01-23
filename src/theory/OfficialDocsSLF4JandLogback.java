4ef5f9c969a12c4b97e8d6afff8465cfc0893758
Samuel <samugit.projects@gmail.com>
2024-01-22 23:35:15 -0300

    Aula06 Acesso à documentação oficial SLF4J e Logback

---
+++ src/theory/OfficialDocsSLF4JandLogback.java
@@ -0,0 +1,23 @@
+package theory;
+
+@SuppressWarnings("ALL")
+public class OfficialDocsSLF4JandLogback {
+
+    /*
+    *
+    * Professor explicou que em caso de necessidade de consultar mais informaçãos dos SLF4J e Logback consultar
+    *
+    * no caso do SLF4J em
+    * https://www.slf4j.org
+    * Clicando no logo superior acessar Documentation / User Manual
+    * Onde é possível inclusive estudar procedimentos avançados que não serão abordados no curso
+    *
+    * no caso do Logback em
+    * https://logback.qos.ch
+    * no momento da visita as páginas eram idênticas, clicar no logo superior / Documentation / The logback manual
+    * Está dividido em 13 capítulos
+    *
+    *
+    *
+    * */
+}
